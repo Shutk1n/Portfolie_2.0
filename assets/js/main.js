80bb5f1ba799799991afebb61d098d6d459320ec
Your Name <Shutk1n>
2023-01-15 17:57:19 +0200

    light/dark mode done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
+// //  Проверка темной темы в localStorage
+// if (localStorage.getItem('darkMode') === 'dark') {
+//   btnDarkMode.classList.add('dark-mode-btn--active');
+//   document.body.classList.add('dark')
+// }
+
+//  Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  btnDarkMode.classList.add('dark-mode-btn--active');
+  document.body.classList.add('dark')
+}
+
+
+//  Включение ночного режима по кнопке
 btnDarkMode.onclick = function () {
   btnDarkMode.classList.toggle("dark-mode-btn--active");
-  document.body.classList.toggle('dark');
+  const isDark = document.body.classList.toggle('dark');
+
+  if (isDark) {
+    localStorage.setItem('darkMode', 'dark');
+  } else {
+    localStorage.setItem('darkMode', 'light');
+  }
 }
 
